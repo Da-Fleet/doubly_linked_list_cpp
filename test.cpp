8d9e31fe6d5b71eef57ecf29b92a3e998966d119
IronBeardX <alejandroyeroval@gmail.com>
2022-10-17 20:41:22 -0400

    Node Bugs fixing

--- test.cpp
+++ test.cpp
@@ -5,8 +5,33 @@ using namespace std;
 
 int main(){
     //[ ]: Testing Node Class
-    //[ ]: Node default constructor
-    //[ ]: Node copy constructor
+    //[x]: Node default constructor
+    Node<int> node(4);
+    Node<int> node1(3);
+    Node<int> node2(2);
+    Node<int> node3(1);
+    cout<<node.data<<endl;
+
     //[ ]: Node move constructor
-    //[ ]: Testing Linked List Class 
+    Node<int> node4 = move(node);
+    cout<<node4.data<<endl;
+    cout<<node.data<<endl;
+
+    //[ ]: Node move asignment operator
+    node4 = move(node1);
+    cout<<node4.data<<endl;
+    cout<<node1.data<<endl;
+
+    //[ ]: Testing Linked List Class
+    //[ ]: Linked List default constructor
+    LinkedList<int> list;
+    cout<<"Empty Constructor"<<endl;
+    list.Print();
+    //[ ]: Linked List move constructor
+    
+    //[ ]: Linked List AddLast
+    //[ ]: Linked List RemoveLast
+    //[ ]: Linked List At
+    //[ ]: Linked List RemoveAt
+    return 0;
 }
